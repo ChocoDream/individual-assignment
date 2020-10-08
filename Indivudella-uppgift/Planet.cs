8f8aeb754315e3ae3be4d26748ec33aa4c3b80be
DanielHss <dh99@live.se>
2020-10-08 13:35:47 +0200

    Init first class "Planet"

---
+++ Indivudella-uppgift/Planet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indivudella_uppgift
+{
+    class Planet
+    {
+        public string name
+        { get;}
+        public Planet(string name)
+        {
+            this.name = name;
+        }
+    }
+}
--- Indivudella-uppgift/Program.cs
+++ Indivudella-uppgift/Program.cs
@@ -11,9 +11,9 @@ namespace Indivudella_uppgift
             {
                 input = Console.ReadLine();
                 Console.WriteLine("User wrote: {0}", input);
+                Planet earth = new Planet(input);
+                Console.WriteLine("Planet {0}", earth.name);
             } while (input.ToLower() != "quit");
-
-            Console.WriteLine("Goodbye");
         }
     }
 }
