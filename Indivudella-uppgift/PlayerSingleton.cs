b8e244cb128914f9f1e3fcf07f5da76d4e964e66
DanielHss <dh99@live.se>
2020-10-08 13:36:12 +0200

    Init PlayerSingleton to handle PlayerInformation

---
+++ Indivudella-uppgift/PlayerSingleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indivudella_uppgift
+{
+    class PlayerSingleton
+    {
+        private PlayerSingleton() { }
+        private static PlayerSingleton _instance;
+        private int money = 100;
+
+        public static PlayerSingleton getInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new PlayerSingleton();
+            }
+            return _instance;
+        }
+    }
+}
