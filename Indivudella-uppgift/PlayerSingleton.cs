bb2e615bc8e0e42cbd65c3e0c0130e83b6365dec
DanielHss <dh99@live.se>
2020-10-11 01:11:55 +0200

    Updated PlayerSingleton

--- Indivudella-uppgift/PlayerSingleton.cs
+++ Indivudella-uppgift/PlayerSingleton.cs
@@ -1,27 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Indivudella_uppgift
 {
-    class PlayerSingleton
+    internal class PlayerSingleton
     {
-        private PlayerSingleton() { }
-        private static PlayerSingleton _instance;
-        private static int money = 100;
-
-        public static PlayerSingleton getInstance()
+        private PlayerSingleton()
         {
-            if(_instance == null)
+        }
+
+        private static PlayerSingleton _instance;
+        private static double money = 100;
+        private static List<SpaceBody> spaceBodies = new List<SpaceBody>();
+
+        public static PlayerSingleton GetInstance()
+        {
+            if (_instance == null)
             {
                 _instance = new PlayerSingleton();
             }
             return _instance;
         }
-        public static int Money
+
+        public static bool isBankrupt()
         {
-            get { return money; }
-            set { money -= value; }
+            return money <= 0 ? true : false;
+        }
+
+        public static void AddMoney(float _money)
+        {
+            money += _money;
+        }
+
+        public static void Pay(float cost)
+        {
+            money -= cost;
+        }
+
+        public static void CheckCurrency()
+        {
+            System.Console.WriteLine("Player's Money: {0}\n", money);
+        }
+
+        public static List<SpaceBody> getSpaceBodies()
+        {
+            return spaceBodies;
         }
     }
 }
