d3156b5ebac5d10a7ed882be8d3b23c8b03a1674
DanielHss <dh99@live.se>
2020-10-11 01:11:17 +0200

    Modified SpaceBodyFactory to work without population written

---
+++ Indivudella-uppgift/SpaceBodyFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indivudella_uppgift
+{
+    static class SpaceBodyFactory
+    {
+
+        public static SpaceBody MakeSpaceBody(string type, string name, float price, string[] resources, int population = 0)
+        {
+            switch (type.ToLower())
+            {
+                case "planet":
+                    return new Planet(type, name, price, resources: resources, population: population);
+                case "asteroid":
+                    return new Asteroid(type, name, price, resources: resources);
+                default:
+                    return null;
+            }
+        }
+    }
+}
